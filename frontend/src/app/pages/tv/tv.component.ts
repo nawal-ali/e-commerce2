16a1f3c160065653bcf7f81f1b426792721473fc
nawal-ali <nmaaa3899@gmail.com>
2025-05-11 17:17:37 +0300

    tv category

--- frontend/src/app/pages/tv/tv.component.ts
+++ frontend/src/app/pages/tv/tv.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../../shared/services/product.service';
 
 @Component({
   selector: 'app-tv',
@@ -7,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './tv.component.css'
 })
 export class TvComponent {
-
+        tv:any;
+        constructor(public productService:ProductService){
+          this.productService.getMonitorCategory().subscribe(res=>{
+            this.tv = res.data;
+          })
+        }
 }
